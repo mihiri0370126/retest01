ac3cabd502028cb9dcacaa2d81e080bf7282fef4
mihiri0370126 <mihiri70451@gmail.com>
2018-06-24 17:33:53 +0000

    Pageコントローラーのviewメソッドを実装

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,5 +2,16 @@
 class Pages extends CI_Controller {
     public function view($page = 'home')
     {
+        if(!file_exists(APPPATH.'views/Pages/'.$page.'.php'))
+        {
+            //Whoops, we don't have a page for that!
+            show_404();
+        }
+        
+        $data['title'] = ucfirst($page); //Capitalizethefirstlett
+        
+        $this->load->view('templates/header',$data);
+        $this->load->view('pages/'.$page,$data);
+        $this->load->view('templates/fotter',$data);
     }
 }
